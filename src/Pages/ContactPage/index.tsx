7642f13d2c89e2a1ad3c16718f789611345ca604
compro-shridhi <shridhi.aggarwal@comprotechnologies.com>
2023-04-18 08:17:59 +0530

    update footer and added contact page text

--- src/Pages/ContactPage/index.tsx
+++ src/Pages/ContactPage/index.tsx
@@ -1,9 +1,111 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Typography from "../../components/Typography";
+import Images from "../../components/Images";
+import TitleSubtitleButtonBox from "../../components/TitleSubtitleButtonBox";
 
-function ContactPage() {
+interface IContactPageProps {}
+
+const ContactSection = styled.section`
+  padding: 80px;
+  position: relative;
+  @media only screen and (max-width: 992px) {
+    padding: 40px 20px;
+  }
+`;
+
+const ContactInfo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ContactInfoIcon = styled.i`
+  color: #da5162;
+  margin-right: 8px;
+`;
+
+const ContactInfoText = styled.p`
+  margin-right: 16px;
+`;
+
+function ContactPage(props: IContactPageProps) {
   return (
-    <div>ContactPage</div>
-  )
+    <ContactSection>
+      <Typography
+        variant="h4"
+        color="#da5162"
+        margin="0 0 1rem 0"
+        textAlign="center"
+      >
+        Get in touch with us
+      </Typography>
+      <Typography variant="body1" margin="0 0 1rem 0" textAlign="center">
+        We'd love to hear from you! Contact us with any questions or comments.
+      </Typography>
+      <ContactInfo>
+        <ContactInfoIcon
+          className="fa fa-map-marker"
+          aria-hidden="true"
+        ></ContactInfoIcon>
+        <ContactInfoText>Block B, Kamla Nagar, Delhi, India</ContactInfoText>
+        <ContactInfoIcon
+          className="fa fa-phone"
+          aria-hidden="true"
+        ></ContactInfoIcon>
+        <ContactInfoText>+91 9876543210</ContactInfoText>
+        <ContactInfoIcon
+          className="fa fa-clock-o"
+          aria-hidden="true"
+        ></ContactInfoIcon>
+        <ContactInfoText>Mon-Sat: 9 A.M. - 5 P.M.</ContactInfoText>
+      </ContactInfo>
+      <div className="contact-form">
+        <div className="container">
+          <form role="form">
+            <div className="row">
+              <div className="col-md-4">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  placeholder="Name"
+                />
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="Email"
+                />
+                <input
+                  type="text"
+                  className="form-control"
+                  id="Subject"
+                  placeholder="Subject"
+                />
+              </div>
+              <div className="col-md-8">
+                {/* <textarea
+                  className="form-control"
+                  id="message"
+                  placeholder="Message"
+                >
+                  {" "}
+                  Message...
+                </textarea> */}
+                <center>
+                  <button type="button" className="form_submit submit-btn">
+                    {" "}
+                    SEND
+                  </button>
+                </center>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </ContactSection>
+  );
 }
 
-export default ContactPage
+export default ContactPage;
--- src/containers/Footer/index.tsx
+++ src/containers/Footer/index.tsx
@@ -1,46 +1,146 @@
-import "./footer.css";
 import Images from "../../components/Images";
+import styled from "styled-components";
+import Typography from "../../components/Typography";
+
+const FooterSection = styled.footer`
+  background-color: #fdf2d2;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  padding: 60px 80px;
+  @media only screen and (max-width: 576px) {
+    padding: 20px;
+  }
+`;
+
+const FooterLogoText = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 3rem;
+  @media only screen and (max-width: 576px) {
+    flex-direction: column;
+  }
+`;
+
+const FooterLogo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-right: 60px;
+  @media only screen and (max-width: 576px) {
+    margin-right: 0px;
+    margin-bottom: 1rem;
+  }
+`;
+
+const FooterText = styled.div``;
+
+const FooterField = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 16px;
+`;
+
+const FooterFieldIcons = styled.i`
+  color: #da5162;
+  margin-right: 12px;
+`;
+
+const FooterSocial = styled.div`
+  @media only screen and (max-width: 576px) {
+    text-align: center;
+  }
+`;
+
+const FooterSocialIcons = styled.i`
+  width: 40px;
+  height: 40px;
+  background-color: #da5162;
+  color: white;
+  text-align: center;
+  padding: 8px;
+  border: 2px solid #da5162;
+  border-radius: 50%;
+  font-size: 1.25rem;
+  margin-right: 20px;
+  transition: all 0.5s;
+  &:hover {
+    background-color: transparent;
+    border: 2px solid #da5162;
+    color: #da5162;
+  }
+`;
+
+const FooterCopyRight = styled(Typography)`
+  @media only screen and (max-width: 576px) {
+    text-align: center;
+  }
+`;
 
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="footerLogoText">
-        <div className="footerLogo">
+    <FooterSection>
+      <FooterLogoText>
+        <FooterLogo>
           <a href="#home">
             <img src={Images.PINK_LOGO} />
           </a>
-          <h3>Bakery!</h3>
-        </div>
-        <div className="footerText">
-          <div className="footerField">
-            <i className="fa fa-map-marker" aria-hidden="true"></i>
-            <p>Block B, Kamla Nagar, Delhi, India</p>
-          </div>
-          <div className="footerField">
-            <i className="fa fa-phone" aria-hidden="true"></i>
-            <p>+91 9876543210</p>
-          </div>
-          <div className="footerField">
-            <i className="fa fa-clock-o" aria-hidden="true"></i>
-            <p>Mon-Sat: 9 A.M. - 5 P.M.</p>
-          </div>
-          <div className="socialIcons">
+          <Typography
+            variant="h5"
+            color="#da5162"
+            fontFamily={`"Pacifico", cursive`}
+            fontWeight="bolder"
+          >
+            Bakery!
+          </Typography>
+        </FooterLogo>
+        <FooterText>
+          <FooterField>
+            <FooterFieldIcons
+              className="fa fa-map-marker"
+              aria-hidden="true"
+            ></FooterFieldIcons>
+            <Typography variant="caption" fontWeight="bold">
+              Block B, Kamla Nagar, Delhi, India
+            </Typography>
+          </FooterField>
+          <FooterField>
+            <FooterFieldIcons
+              className="fa fa-phone"
+              aria-hidden="true"
+            ></FooterFieldIcons>
+            <Typography variant="caption" fontWeight="bold">
+              +91 9876543210
+            </Typography>
+          </FooterField>
+          <FooterField>
+            <FooterFieldIcons
+              className="fa fa-clock-o"
+              aria-hidden="true"
+            ></FooterFieldIcons>
+            <Typography variant="caption" fontWeight="bold">
+              Mon-Sat: 9 A.M. - 5 P.M.
+            </Typography>
+          </FooterField>
+          <FooterSocial>
             <a href="/">
-              <i className="fa fa-facebook"></i>
+              <FooterSocialIcons className="fa fa-facebook"></FooterSocialIcons>
             </a>
             <a href="/">
-              <i className="fa fa-youtube-play"></i>
+              <FooterSocialIcons className="fa fa-youtube-play"></FooterSocialIcons>
             </a>
             <a href="/">
-              <i className="fa fa-twitter"></i>
+              <FooterSocialIcons className="fa fa-twitter"></FooterSocialIcons>
             </a>
-          </div>
-        </div>
-      </div>
-      <div className="footerCopyRight">
-        <p>Copyright © 2023 - Developed by Shridhi</p>
-      </div>
-    </footer>
+          </FooterSocial>
+        </FooterText>
+      </FooterLogoText>
+      <FooterCopyRight variant="caption" fontWeight="bold">
+        Copyright © 2023 - Developed by Shridhi
+      </FooterCopyRight>
+    </FooterSection>
   );
 }
 
