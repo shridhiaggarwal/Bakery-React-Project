43335194033e6df45ccc7274c38f07dbf354675f
shridhiaggarwal <shridhi.aggarwal@gmail.com>
2023-05-01 18:29:54 +0530

    update in Counter/index.tsx

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -2,33 +2,24 @@ import React from "react";
 import CountUp, { CountUpProps } from "react-countup";
 
 function Counter(props: CountUpProps) {
-  const {
-    className,
-    start,
-    end,
-    delay = null,
-    duration = 2,
-    prefix = "",
-    suffix = "",
-  } = props;
+  const { className, start, end, duration = 2, prefix = "" } = props;
 
   return (
     <CountUp
       start={start}
       end={end}
-      delay={delay}
       duration={duration}
       prefix={prefix}
-      suffix={suffix}
       className={className}
-    >
-      {({ countUpRef }) => (
-        <div>
-          <span ref={countUpRef} className={className} />
-        </div>
-      )}
-    </CountUp>
+    />
   );
 }
 
 export default Counter;
+
+//   {({ countUpRef }) => (
+//     <div>
+//       <span ref={countUpRef} className={className} />
+//     </div>
+//   )}
+// </CountUp>
